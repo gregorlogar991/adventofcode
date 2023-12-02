32dbc396b3b91e4f487b441af4b48e7db4ca4df2
Gregor Logar <gregorlogar991@gmail.com>
2023-12-02 09:29:34 +0100

    Add first solution

---
+++ 2023/02/1.py
@@ -0,0 +1,20 @@
+with open("data.txt") as file:
+    data = file.read()
+bag_contain = {"red": 12, "green": 13, "blue": 14}
+
+sum = 0
+for line in data.split("\n"):
+    possible = True
+    print("line: ", line.split(":")[1].split(";"))
+    for games in line.split(":")[1].split(";"):
+        print("game: ", games)
+        for game in games.split(","):
+            if (
+                int(game.lstrip().split(" ")[0])
+                > bag_contain[game.lstrip().split(" ")[1]]
+            ):
+                possible = False
+    if possible:
+        sum += int(line.split(":")[0].replace("Game ", ""))
+
+print(sum)
