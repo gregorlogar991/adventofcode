38a21a913ff7de7a49ddb81380ef8099c3bbe52e
Gregor Logar <gregorlogar991@gmail.com>
2023-12-03 12:31:06 +0100

    0101

--- 2023/01/0101.py
+++ 2023/01/0101.py
@@ -6,7 +6,6 @@ with open("data.txt") as file:
 solution = 0
 for line in data.strip().split():
     numbers = re.findall("\\d", line)
-    print(numbers, " ", int(numbers[0]) * 10 + int(numbers[-1]))
-    solution += int(numbers[0]) * 10 + int(numbers[-1])
+    solution += int(numbers[0] + numbers[-1])
 
 print("Solution: ", solution)
